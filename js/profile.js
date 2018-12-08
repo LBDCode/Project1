ee7a5d03c72d8ef10c0cdef100571b99d5c92b1f
Mr Scary <ronwazny@gmail.com>
2018-12-08 13:30:30 -0500

    results

--- js/profile.js
+++ js/profile.js
@@ -1,72 +1,83 @@
 var config = {
-    apiKey: "AIzaSyBScGiSeR7W0ZUytnqnnze45isXpj6yBv0",
-    authDomain: "beerstrology.firebaseapp.com",
-    databaseURL: "https://beerstrology.firebaseio.com",
-    projectId: "beerstrology",
-    storageBucket: "beerstrology.appspot.com",
-    messagingSenderId: "309263077148"
-  };
-  
+  apiKey: "AIzaSyBScGiSeR7W0ZUytnqnnze45isXpj6yBv0",
+  authDomain: "beerstrology.firebaseapp.com",
+  databaseURL: "https://beerstrology.firebaseio.com",
+  projectId: "beerstrology",
+  storageBucket: "beerstrology.appspot.com",
+  messagingSenderId: "309263077148"
+};
+
 firebase.initializeApp(config);
 
 var database = firebase.database();
 
 firebase.auth().onAuthStateChanged(user => {
-    console.log(user);
-    
-    if(user) {
-        $("#submit-user").on("click", function() {
-            event.preventDefault();
-            //will need an in statement to check if userName already exists?
-            //check if 21
-            var firstName = $("#first-name").val().trim();
-            var lastName = $("#last-name").val().trim();
-            var birthday = $("#birthday").val().trim();
-            var persStatement = $("#personal-statement").val().trim();
-            var userName = $("#user-name").val().trim();
-            var user = firebase.auth().currentUser;
+  console.log(user);
 
-            console.log(user);
+  if (user) {
+    $("#submit-user").on("click", function() {
+      event.preventDefault();
+      //will need an in statement to check if userName already exists?
+      //check if 21
+      var firstName = $("#first-name")
+        .val()
+        .trim();
+      var lastName = $("#last-name")
+        .val()
+        .trim();
+      var birthday = $("#birthday")
+        .val()
+        .trim();
+      var persStatement = $("#personal-statement")
+        .val()
+        .trim();
+      var userName = $("#user-name")
+        .val()
+        .trim();
+      var user = firebase.auth().currentUser;
 
-            console.log(firstName, lastName, birthday, persStatement, userName);
+      console.log(user);
 
-            //update user info w/ userName and maybe picture            
+      console.log(firstName, lastName, birthday, persStatement, userName);
 
-            user.updateProfile({
-                displayName: userName,
-                // photoURL: "https://example.com/jane-q-user/profile.jpg"
-            }).then(function() {
-                // Update successful.
-                //create custom profile
-                createProfile(firstName, lastName, birthday, userName, persStatement);
-                //redirect to results page
-                window.location = "response.html";
-            }).catch(function(error) {
-                // An error happened.
-                console.log("Err: " + error)
-            });   
-                        
+      //update user info w/ userName and maybe picture
+
+      user
+        .updateProfile({
+          displayName: userName
+          // photoURL: "https://example.com/jane-q-user/profile.jpg"
         })
-        
-    } else{
-        // console.log("Signed out");
-        // window.location = "login.html";
-    }
-
+        .then(function() {
+          // Update successful.
+          //create custom profile
+          createProfile(firstName, lastName, birthday, userName, persStatement);
+          //redirect to results page
+          window.location = "results.html";
+        })
+        .catch(function(error) {
+          // An error happened.
+          console.log("Err: " + error);
+        });
+    });
+  } else {
+    // console.log("Signed out");
+    // window.location = "login.html";
+  }
 });
 
 function createProfile(first, last, bday, displayName, statement) {
-    firebase.database().ref('users/' + displayName).set({
-        firstName: first,
-        lastName: last,
-        birthday: bday,
-        personalStatement: statement
-        // profile_picture : imageUrl,
-        // Add more stuff here
+  firebase
+    .database()
+    .ref("users/" + displayName)
+    .set({
+      firstName: first,
+      lastName: last,
+      birthday: bday,
+      personalStatement: statement
+      // profile_picture : imageUrl,
+      // Add more stuff here
     });
 }
 
 //function to assign sign
 //how do you upload photos
-
-
