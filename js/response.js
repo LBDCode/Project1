6bf1c380d1a3132ba4c059a8057fb21404417627
Laura Gillespie <lauragillespie@Lauras-MacBook-Pro.local>
2018-12-13 19:46:11 -0500

    pushing updates

--- js/response.js
+++ js/response.js
@@ -1,41 +1,35 @@
 var config = {
-    apiKey: "AIzaSyBScGiSeR7W0ZUytnqnnze45isXpj6yBv0",
-    authDomain: "beerstrology.firebaseapp.com",
-    databaseURL: "https://beerstrology.firebaseio.com",
-    projectId: "beerstrology",
-    storageBucket: "beerstrology.appspot.com",
-    messagingSenderId: "309263077148"
-  };
-  
+  apiKey: "AIzaSyBScGiSeR7W0ZUytnqnnze45isXpj6yBv0",
+  authDomain: "beerstrology.firebaseapp.com",
+  databaseURL: "https://beerstrology.firebaseio.com",
+  projectId: "beerstrology",
+  storageBucket: "beerstrology.appspot.com",
+  messagingSenderId: "309263077148"
+};
+
 firebase.initializeApp(config);
 
 var database = firebase.database();
 
-
 firebase.auth().onAuthStateChanged(user => {
+  if (user) {
+    var user = firebase.auth().currentUser;
+    console.log(user);
 
-    if(user) {
+    var displayName = firebase.auth().currentUser.displayName;
 
-        var user = firebase.auth().currentUser;
-        console.log(user);
+    $("#response-user-name").text(displayName);
 
-        var displayName = firebase.auth().currentUser.displayName;
-
-        $("#response-user-name").text(displayName);
-
-        $("#signout-user").on("click", event => {
-
-            firebase.auth().signOut();
-            //put a redirect to signout page here
-                
-        });
-        
-    } else{
-        console.log("Signed out");
-        window.location = "login.html";
-    }
-
-
-    
-
+    $("#signout-user").on("click", event => {
+      firebase.auth().signOut();
+      //put a redirect to signout page here
+    });
+  } else {
+    console.log("Signed out");
+    window.location = "login.html";
+  }
+  $(".logout-button").on("click", event => {
+    firebase.auth().signOut();
+    window.location = "login.html";
+  });
 });
